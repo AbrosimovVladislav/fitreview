22051bf9f5933e6b10c9a3511e5dec6dd7a0a5a4
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2024-08-15 20:07:11 +0200

    main: init

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import {Image, ScrollView, Text, View} from 'react-native'
+import React from 'react'
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Redirect, router} from "expo-router"
+
+import {StatusBar} from "expo-status-bar";
+import {useGlobalContext} from "@/context/GlobalProvider";
+
+const RootLayout = () => {
+
+    const {isLoading, isLoggedIn} = useGlobalContext();
+
+    if(!isLoading && isLoggedIn) return <Redirect href='/home'/>
+
+    return (
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{
+                height: '100%'
+            }}>
+                <View className="w-full justify-center items-center min-h-[85vh] px-4">
+                 <Text>
+                     Hello
+                 </Text>
+                </View>
+            </ScrollView>
+
+            <StatusBar
+                backgroundColor='#161622'
+                style='light'
+            />
+        </SafeAreaView>
+    )
+}
+export default RootLayout
