02c1d6cb8076a1180dbe3fda7fd5c9e95b097582
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2024-09-13 11:38:42 +0200

    main: waiting for results page

--- app/(tabs)/review.tsx
+++ app/(tabs)/review.tsx
@@ -1,5 +1,5 @@
 import {View, Text, ScrollView, Image} from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 import PageHeader from "@/components/PageHeader";
 import {router} from "expo-router";
@@ -9,6 +9,8 @@ import {Ionicons} from "@expo/vector-icons";
 import Button from "@/components/common/Button";
 
 const Review = () => {
+
+    const [status, setStatus] = useState('WaitingResults');
 
     const fitReviewParams = [
         'Lorem Ipsum is simply dummy text of printing',
@@ -21,51 +23,82 @@ const Review = () => {
 
     }
 
+    const initialFRScreen = <ScrollView>
+        <View className='pt-4'>
+            <PageHeader title='Fit Review'/>
+        </View>
+
+        <View className='flex flex-col pt-2'>
+            <View name='image-area' className='flex justify-center items-center rounded-xl'>
+                <Image
+                    source={images.cards}
+                    className='max-w-[400px] h-[260px] rounded-xl '
+                    resizeMode='contain'
+                />
+            </View>
+
+            <View name='title-and-description-area' className='flex flex-col justify-center items-center pt-4'>
+                <Text className='text-md text-gray-300 font-mmedium pt-2 pb-6 text-center'>
+                    It is a long established fact that a reader
+                    will be distracted by the readable
+                </Text>
+                {
+                    fitReviewParams.map(param => {
+                        return <View key={Math.random()} className='flex flex-row justify-center items-center pt-3'>
+                            <Ionicons name='checkmark-done' size={22} color='#FF9001'/>
+                            <Text className={'text-gray-100 pt-1 pl-2 font-msemibold'}>
+                                {param}
+                            </Text>
+                        </View>
+                    })
+                }
+            </View>
+
+            <View name='button' className='pt-10 px-4'>
+                <Button
+                    title="Get Started"
+                    onPress={() => router.push('/review/payment')}
+                    containerStyles="mt-2"
+                    icon={'caret-forward'}
+                />
+            </View>
+        </View>
+
+
+    </ScrollView>
+    const waitingForResultsScreen = <ScrollView>
+        <PageHeader/>
+        <View className='flex justify-center items-center gap-16 p-4'>
+            <View className='flex gap-3 justify-center items-center'>
+                <Text className="text-4xl text-gray-300 text-semibold mt-4 font-cbebas">
+                    Perfect !
+                </Text>
+                <Text className="text-md text-center text-gray-100 font-mregular">
+                    Your request is received. We’ll analyze your data and share insights soon
+                </Text>
+            </View>
+            <Image
+                source={images.cards}
+                className='max-w-[420px] h-[280px] rounded-xl '
+                resizeMode='contain'
+            />
+            <View className='flex gap-2'>
+                <Text className="text-xl text-center text-secondary-100 font-cbebas">
+                    Discover your potential
+                </Text>
+                <Text className="text-md text-center text-gray-100 font-mmedium">
+                    We’ll help you understand your body and reach your goals effectively
+                </Text>
+            </View>
+
+        </View>
+    </ScrollView>
+
     return (
         <SafeAreaView className='bg-primary h-full'>
-            <ScrollView>
-                <View className='pt-4'>
-                    <PageHeader title='Fit Review'/>
-                </View>
-
-                <View className='flex flex-col pt-2'>
-                    <View name='image-area' className='flex justify-center items-center rounded-xl'>
-                            <Image
-                                source={images.cards}
-                                className='max-w-[400px] h-[260px] rounded-xl '
-                                resizeMode='contain'
-                            />
-                    </View>
-
-                    <View name='title-and-description-area' className='flex flex-col justify-center items-center pt-4'>
-                        <Text className='text-md text-gray-300 font-mmedium pt-2 pb-6 text-center'>
-                            It is a long established fact that a reader
-                            will be distracted by the readable
-                        </Text>
-                        {
-                            fitReviewParams.map(param => {
-                                return <View key={Math.random()} className='flex flex-row justify-center items-center pt-3'>
-                                    <Ionicons name='checkmark-done' size={22} color='#FF9001'/>
-                                    <Text className={'text-gray-100 pt-1 pl-2 font-msemibold'}>
-                                        {param}
-                                    </Text>
-                                </View>
-                            })
-                        }
-                    </View>
-
-                    <View name='button' className='pt-10 px-4'>
-                        <Button
-                            title="Get Started"
-                            onPress={() => router.push('/review/payment')}
-                            containerStyles="mt-2"
-                            icon={'caret-forward'}
-                        />
-                    </View>
-                </View>
-
-
-            </ScrollView>
+            {status === 'NotStarted'
+                ? initialFRScreen
+                : waitingForResultsScreen}
         </SafeAreaView>
     )
 }
