72111716a6b35e980821fccf11afc33e68448265
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2024-10-17 18:14:35 +0200

    review: fix of not centered pictures in ListItem component

--- components/common/ListItem.tsx
+++ components/common/ListItem.tsx
@@ -5,16 +5,18 @@ import {router} from "expo-router";
 const ListItem = ({title, image, description, route}) => {
 
     return (
-        <View className=''>
+        <View className='w-full px-1 py-1'>
             <TouchableOpacity onPress={() => router.push(route)}>
                 <Image
                     source={{uri: image}}
-                    className='max-w-[350px] h-[200px] rounded-xl'
+                    className='w-full aspect-video rounded-xl'
                     resizeMode='contain'
                 />
             </TouchableOpacity>
-            <Text className='text-xl text-gray-300 font-mbold'>{title}</Text>
-            <Text className='text-sm text-gray-300 font-mmedium pt-1 pb-8'>{description}</Text>
+            <View className='w-full pl-0.5 pt-1'>
+                <Text className='text-xl text-gray-300 font-mbold'>{title}</Text>
+                <Text className='text-sm text-gray-300 font-mmedium pt-1 pb-8'>{description}</Text>
+            </View>
         </View>
     )
 }
