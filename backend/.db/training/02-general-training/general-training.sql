bf98c95c699abc188d60f496bbfad264071541b2
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2024-10-30 12:38:10 +0100

    be: additions to sql and http scripts

---
+++ backend/.db/training/02-general-training/general-training.sql
@@ -0,0 +1,6 @@
+INSERT INTO training (id, description, russian_title, thumbnail, title)
+VALUES (1,
+        'MFR hips training for basic warm-up and mobility',
+        'Тренировка для МФР бедер',
+        'https://cloud.appwrite.io/v1/storage/buckets/66c331f000314ec68775/files/66f1c65e0012672775a9/view?project=66c32ed800357b5e7314&project=66c32ed800357b5e7314',
+        'MFR Hips Training');
---
+++ backend/.db/training/03-training-exercise/training-exercise.sql
@@ -0,0 +1,8 @@
+INSERT INTO training_exercise (training_id, exercise_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5),
+    (1, 6);
