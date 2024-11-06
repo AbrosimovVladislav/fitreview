d94f520a5a5303350c1308d8c76d014d8e8944e1
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2024-11-06 19:39:18 +0100

    review: Визуальные доработки в areas на саммари табе

--- mobile/components/common/Accordion.tsx
+++ mobile/components/common/Accordion.tsx
@@ -10,7 +10,7 @@ const Accordion = ({title, content}) => {
     return (
         <View className='pt-2 pb-3'>
             <TouchableOpacity className='flex-row justify-between items-center' onPress={() => setIsCollapsed(!isCollapsed)}>
-                <Text className='text-2xl pl-1 pt-1 text-white text-semibold font-cbebas'>
+                <Text className='text-2xl pl-1 pb-2 text-white text-semibold font-cbebas'>
                     {title}
                 </Text>
                 <Ionicons name={isCollapsed ? 'chevron-down' : 'chevron-up'} size={26} color='white'/>
