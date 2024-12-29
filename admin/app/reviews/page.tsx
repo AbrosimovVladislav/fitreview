3f957e22a0bfc60058a28c2b3846ed63b2fe692b
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2024-12-29 13:40:14 +0100

    admin: init

---
+++ admin/app/reviews/page.tsx
@@ -0,0 +1,10 @@
+export default function ReviewsPage() {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold mb-4">Reviews</h1>
+            <div className="bg-white shadow rounded-lg p-6">
+                <p className="text-gray-500">Here will be the list of reviews.</p>
+            </div>
+        </div>
+    );
+}
