54355e1a9fb4e51b9a4b80a6a54cf84d6f0c1db4
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2025-02-20 10:52:49 +0100

    dev: admin deploy step 11 - minor fix

--- admin/service/beClient.ts
+++ admin/service/beClient.ts
@@ -1,4 +1,5 @@
 // const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+//
 const BASE_URL = "https://fit-review-444116-633533964999.europe-north1.run.app";
 
 export const beClient = {
