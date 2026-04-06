5f8e0efdd6228dfc3ccf31b9c17609ca414739cb
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2026-04-06 09:56:29 +0200

    reborn-1.1

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -8,7 +8,7 @@ export default ({ config }) => {
             },
             API_URL:
                 process.env.EXPO_ENV === 'local'
-                    ? 'http://localhost:3000/api/v1'
+                    ? 'http://localhost:8080/api/v1'
                     : process.env.EXPO_ENV === 'test'
                         ? 'https://fit-review-444116-633533964999.europe-north1.run.app/api/v1'
                         : 'https://fit-review-444116-633533964999.europe-north1.run.app/api/v1',
